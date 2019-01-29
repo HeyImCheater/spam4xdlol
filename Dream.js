b2abfc688fa95b6215681f0978d88a63ef7df838
HeyImCheater <47138581+HeyImCheater@users.noreply.github.com>
2019-01-29 12:03:43 +0300

    Add files via upload

---
+++ Dream.js
@@ -0,0 +1,14 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("HeyImMnK_! - #SatanMC-Staff.");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("539444955366817804")
+setInterval(function() {
+channel.send(`SoSo Need Credits **Spaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaam ] **`);
+}, 25)
+})
+ 
+ 
+client.login("NTI2MDM1ODk0MDcxOTg0MTMy.DzB6Sw.A2Ifbj2ncsWhVkQFuYvOkZl42yo");
